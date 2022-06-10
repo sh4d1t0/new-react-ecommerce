d3cb9bb25e517ce231eb3baf03b2cba8de1ace19
sh4d1t0 <shadito@live.com.mx>
2022-06-10 02:55:15 -0500

    fix react router dom webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../public'),
     filename: '[name].[contenthash].js',
-    publicPath: '',
+    publicPath: '/',
   },
   plugins: [
     new CleanWebpackPlugin(),
